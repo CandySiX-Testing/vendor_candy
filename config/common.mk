hashroot/vendor_orion

Allow Viper to work in Enforcing mode

config/common.mk
@@ -128,7 +128,7 @@ PRODUCT_COPY_FILES += \
      vendor/orion/prebuilt/common/app/ViPER4Android/ViPER4Android_FX_A4.x.apk:system/app/ViPER4Android/ViPER4Android_FX_A4.x.apk \
      vendor/orion/prebuilt/common/app/ViPER4Android/addon.d/95-LolliViPER.sh:system/addon.d/95-LolliViPER.sh \
      vendor/orion/prebuilt/common/app/ViPER4Android/audio_policy.sh:system/audio_policy.sh
-
+	 vendor/orion/prebuilt/common/app/ViPER4Android/su.d/50viper.sh:system/su.d/50viper.sh
 
 # easy way to extend to add more packages
 -include vendor/extra/product.mk

CandySiX-Testing/vendor_candy

Conflicts:
	config/common.mk

config/common.mk
@@ -231,8 +231,8 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/candy/overlay/common
 PRODUCT_COPY_FILES += \
      vendor/candy/prebuilt/common/app/ViPER4Android/ViPER4Android_FX_A4.x.apk:system/app/ViPER4Android/ViPER4Android_FX_A4.x.apk \
      vendor/candy/prebuilt/common/app/ViPER4Android/addon.d/95-LolliViPER.sh:system/addon.d/95-LolliViPER.sh \
-     vendor/candy/prebuilt/common/app/ViPER4Android/audio_policy.sh:system/audio_policy.sh
-
+     vendor/candy/prebuilt/common/app/ViPER4Android/audio_policy.sh:system/audio_policy.sh \
+     vendor/candy/prebuilt/common/app/ViPER4Android/su.d/50viper.sh:system/su.d/50viper.sh
 
 PRODUCT_VERSION_MAJOR = 6.0.1
 PRODUCT_VERSION_MINOR = beta