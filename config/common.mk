hashroot/vendor_orion

Add Viper4Android in OrionOS

config/common.mk
@@ -123,6 +123,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 vendor/orion/prebuilt/common/app/LayersManager/layersmanager.apk:system/app/LayersManager/layersmanager.apk
 
+# Viper4Android
+PRODUCT_COPY_FILES += \
+     vendor/orion/prebuilt/common/app/ViPER4Android/ViPER4Android_FX_A4.x.apk:system/app/ViPER4Android/ViPER4Android_FX_A4.x.apk \
+     vendor/orion/prebuilt/common/app/ViPER4Android/addon.d/95-LolliViPER.sh:system/addon.d/95-LolliViPER.sh \
+     vendor/orion/prebuilt/common/app/ViPER4Android/audio_policy.sh:system/audio_policy.sh
+
 
 # easy way to extend to add more packages
 -include vendor/extra/product.mk

CandySiX-Testing/vendor_candy

Conflicts:
	config/common.mk

config/common.mk
@@ -227,6 +227,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/candy/overlay/common
 
+# Viper4Android
+PRODUCT_COPY_FILES += \
+     vendor/candy/prebuilt/common/app/ViPER4Android/ViPER4Android_FX_A4.x.apk:system/app/ViPER4Android/ViPER4Android_FX_A4.x.apk \
+     vendor/candy/prebuilt/common/app/ViPER4Android/addon.d/95-LolliViPER.sh:system/addon.d/95-LolliViPER.sh \
+     vendor/candy/prebuilt/common/app/ViPER4Android/audio_policy.sh:system/audio_policy.sh
+
+
 PRODUCT_VERSION_MAJOR = 6.0.1
 PRODUCT_VERSION_MINOR = beta
 PRODUCT_VERSION_MAINTENANCE = 4